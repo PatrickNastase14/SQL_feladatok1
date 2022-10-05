938b498587e0d5a9a444ba5903f8976470b53750
PatrickNastase14 <55185211+PatrickNastase14@users.noreply.github.com>
2022-10-05 11:09:17 +0200

    Update sql_feladatok.sql

--- sql_feladatok.sql
+++ sql_feladatok.sql
@@ -11,7 +11,7 @@ SELECT COUNT(*) FROM employees WHERE city="Seattle";
 SELECT COUNT(*) FROM employees WHERE job_title='Sales Rpresentativ';
 SELECT COUNT(*) FROM employees WHERE first_name LIKE "A%";
 -- 4feladat
-SELECT Min(standard_cost) FROM products;
+SELECT MIN(standard_cost) FROM products;
 SELECT MAX(standard_cost) FROM products;
 SELECT MAX(standard_cost) FROM products WHERE list_price >= 30;
 SELECT MAX(standard_cost) FROM products WHERE product_code Like '%CO%';
