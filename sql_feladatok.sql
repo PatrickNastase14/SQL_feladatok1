aa87dfbecd76c00d56635ad30e36b4ac8a892b56
PatrickNastase14 <55185211+PatrickNastase14@users.noreply.github.com>
2022-10-05 11:11:14 +0200

    Update sql_feladatok.sql

--- sql_feladatok.sql
+++ sql_feladatok.sql
@@ -14,4 +14,4 @@ SELECT COUNT(*) FROM employees WHERE first_name LIKE "A%";
 SELECT MIN(standard_cost) FROM products;
 SELECT MAX(standard_cost) FROM products;
 SELECT MAX(standard_cost) FROM products WHERE list_price >= 30;
-SELECT MAX(standard_cost) FROM products WHERE product_code Like '%CO%';
+SELECT MAX(standard_cost) FROM products WHERE product_code LIKE '%CO%';
